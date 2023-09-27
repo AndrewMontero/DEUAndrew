f3f0e5b62f6d6a446e5d76d82a24f4c7ca086475
AndrewMontero <andrewmontero0805@gmail.com>
2023-09-27 07:57:45 -0600

    Taller de clase

---
+++ Taller/src/taller/Taller.java
@@ -0,0 +1,18 @@
+
+package taller;
+
+import GUI.*;
+
+public class Taller {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GUI gui=new GUI ();
+        gui.setVisible(true);
+        
+    }
+    
+}
